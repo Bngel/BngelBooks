6b1c15ea0f75755c29d3a0445d73bc6f7bed6748
bngel <bngel1024@gmail.com>
2020-11-28 02:01:32 +0800

    Switch开关随心记功能

---
+++ app/src/main/java/com/example/bngelbooks/logic/network/RecordService.kt
@@ -0,0 +1,56 @@
+package com.example.bngelbooks.logic.network
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.bngelbooks.MainActivity
+import com.example.bngelbooks.R
+import com.example.bngelbooks.ui.AddOrderLayout.AddOrderActivity
+
+class RecordService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+            as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("record_service","Record",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        val record_intent = Intent(this,AddOrderActivity::class.java)
+        record_intent.putExtra("from_Service",true)
+
+        val notification = NotificationCompat.Builder(this,"record_service")
+            .setContentIntent((PendingIntent
+                .getActivity(this,0,record_intent,0)))
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.rem_icon))
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("记账")
+            .setContentText("立即打开记账界面")
+            .setWhen(System.currentTimeMillis())
+            .build()
+
+        startForeground(1,notification)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopForeground(true)
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
--- app/src/main/java/com/example/bngelbooks/ui/MeSettings/SettingActivity.kt
+++ app/src/main/java/com/example/bngelbooks/ui/MeSettings/SettingActivity.kt
@@ -2,9 +2,11 @@ package com.example.bngelbooks.ui.MeSettings
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bngelbooks.R
+import com.example.bngelbooks.logic.network.RecordService
 import com.example.bngelbooks.ui.WidgetSetting
 import com.suke.widget.SwitchButton
 import kotlinx.android.synthetic.main.activity_setting.*
@@ -31,6 +33,12 @@ class SettingActivity : AppCompatActivity() {
         msgRecordSwitch.isEnabled = true //disable button
         msgRecordSwitch.setEnableEffect(true) //disable the switch animation
         msgRecordSwitch.setOnCheckedChangeListener(SwitchButton.OnCheckedChangeListener { view, isChecked ->
+            val service_intent = Intent(this,RecordService::class.java)
+            if (isChecked)
+                startService(service_intent)
+            else
+                stopService(service_intent)
+
             getSharedPreferences("SwitchStatus", Context.MODE_PRIVATE).edit().apply {
                 putBoolean("record",isChecked)
                 apply()
